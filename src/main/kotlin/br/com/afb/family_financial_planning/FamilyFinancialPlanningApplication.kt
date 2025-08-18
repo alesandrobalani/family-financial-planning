e0fbcb124913d9526f992054b7b47f3004baeb9b
Alesandro Facina Balani <afbalani@hotmail.com>
2025-08-17 22:01:51 -0300

    initial code

---
+++ src/main/kotlin/br/com/afb/family_financial_planning/FamilyFinancialPlanningApplication.kt
@@ -0,0 +1,11 @@
+package br.com.afb.family_financial_planning
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FamilyFinancialPlanningApplication
+
+fun main(args: Array<String>) {
+	runApplication<FamilyFinancialPlanningApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/afb/family_financial_planning/FamilyFinancialPlanningApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.afb.family_financial_planning
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FamilyFinancialPlanningApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
